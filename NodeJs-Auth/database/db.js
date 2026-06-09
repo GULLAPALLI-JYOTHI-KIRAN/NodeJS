46255c5110ee04b0b0065733119a63b6222325c4
GULLAPALLI JYOTHI KIRAN <79632709+GULLAPALLI-JYOTHI-KIRAN@users.noreply.github.com>
2026-06-09 11:40:46 +0530

    Add database connection setup using mongoose

---
+++ NodeJs-Auth/database/db.js
@@ -0,0 +1,17 @@
+const mongoose=require("mongoose");
+
+const connectToDB=async ()=>{
+    try{
+
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("Database connected successfully.")
+
+    }catch(e){
+        console.log("Failed to connect to DB",e);
+        process.exit(1);
+
+        
+    }
+}
+
+module.exports=connectToDB
