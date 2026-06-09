668ba96971f198a2e98cf56cfeb9a8f0535f3b62
GULLAPALLI JYOTHI KIRAN <79632709+GULLAPALLI-JYOTHI-KIRAN@users.noreply.github.com>
2026-06-09 11:27:01 +0530

    Add wraper-demo.js to log messages and greet

---
+++ node-modules-system/wraper-demo.js
@@ -0,0 +1,10 @@
+console.log("This is wraper-demo ");
+
+
+const wraperExplore=require("./wraper-explore");
+
+
+console.log("__filename in wraper-demo",__filename)
+console.log("__direname in wraper-demo",__dirname)
+
+wraperExplore.greet("Good morning All ,I am Jyothi Kiran.")
